2f49a924a831472f7e76103052d1115d583329c8
Eric Widjaja <ericwidjaja@pursuit.org>
2019-10-05 23:43:48 -0400

    created JournalEntryImagePicker VC file, connected TextView to the VC

---
+++ PhotoJournal-Assignment/PhotoJournal-Assignment/Controllers/JournalEntryImagePickerViewController.swift
@@ -0,0 +1,37 @@
+//
+//  JournalEntryImagePickerViewController.swift
+//  PhotoJournal-Assignment
+//
+//  Created by Eric Widjaja on 10/5/19.
+//  Copyright © 2019 Eric Widjaja. All rights reserved.
+//
+
+import UIKit
+
+class JournalEntryImagePickerViewController: UIViewController {
+    
+    
+    @IBOutlet weak var journalCaptionTextView: UITextView!
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
