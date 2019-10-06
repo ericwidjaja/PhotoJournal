e72845792f017724ade2aa0a5cccc2c71f795a30
Eric Widjaja <ericwidjaja@pursuit.org>
2019-10-05 21:57:52 -0400

    Re-created Assignment from scratch, added README file and organized files inside the project

---
+++ PhotoJournal-Assignment/PhotoJournal-Assignment/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoJournal-Assignment
+//
+//  Created by Eric Widjaja on 10/5/19.
+//  Copyright © 2019 Eric Widjaja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
